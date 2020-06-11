5b67ba3f2b335ba1f5dec54d25a277f4d46bf701
yoannd <yoanndewilde@gmail.com>
2020-06-11 11:21:54 +0200

    Refactor: move shunting_yard function into a separate file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,8 @@
+mod shunting_yard;
 mod tokens;
-use tokens::Token::{self, *};
 
-fn shunting_yard(tokens: Vec<Token>) -> Vec<Token> {
-    let mut op_stack: Vec<Token> = Vec::new();
-    let mut output = Vec::new();
-
-    for tok in &tokens {
-        if tok.is_op() {
-            if let Some(last) = op_stack.last() {
-                if last.prior(tok) {
-                    // Pop all the op stack into output
-                    while let Some(top) = op_stack.pop() {
-                        output.push(top);
-                    }
-                }
-            }
-
-            op_stack.push(*tok);
-        } else {
-            output.push(*tok);
-        }
-    }
-
-    while !op_stack.is_empty() {
-        output.push(op_stack.pop().unwrap());
-    }
-
-    output
-}
+use shunting_yard::shunting_yard;
+use tokens::Token::*;
 
 fn main() {
     // 1 + 2 * 3 - 4
---
+++ src/shunting_yard.rs
@@ -0,0 +1,29 @@
+use crate::tokens::*;
+
+pub fn shunting_yard(tokens: Vec<Token>) -> Vec<Token> {
+    let mut op_stack: Vec<Token> = Vec::new();
+    let mut output = Vec::new();
+
+    for tok in &tokens {
+        if tok.is_op() {
+            if let Some(last) = op_stack.last() {
+                if last.prior(tok) {
+                    // Pop all the op stack into output
+                    while let Some(top) = op_stack.pop() {
+                        output.push(top);
+                    }
+                }
+            }
+
+            op_stack.push(*tok);
+        } else {
+            output.push(*tok);
+        }
+    }
+
+    while !op_stack.is_empty() {
+        output.push(op_stack.pop().unwrap());
+    }
+
+    output
+}

9f8ebf903d625bbc87557baa48c68738554b33fc
yoannd <yoanndewilde@gmail.com>
2020-06-12 17:29:10 +0200

    [Try] 1 - 4 + 2*3

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,7 @@ use tokens::Token::*;
 fn main() {
     // 1 + 2 * 3 - 4
     // [Number(1), Number(2), Number(3), Number(4), Minus, Mul, Plus]
-    let input = vec![
-        Number(1),
-        Minus,
-        Number(4),
-        Plus,
-        Number(2),
-        Mul,
-        Number(3),
-        Mul,
-        Number(3),
-        Mul,
-        Number(3),
-    ];
+    let input = vec![Number(1), Minus, Number(4), Plus, Number(2), Mul, Number(3)];
     let output = shunting_yard(input);
 
     println!("{:?}", output);

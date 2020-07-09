f71fbc0d013d7b732d31f781a5b0d08e36cdc25f
yoannd <yoanndewilde@gmail.com>
2020-07-09 13:53:35 +0200

    Working REPL

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,40 @@ mod rpn;
 mod shunting_yard;
 mod tokens;
 
+use std::io::{self, Write};
+
 use parse_eval::eval;
 use rpn::eval_rpn;
 use shunting_yard::shunting_yard;
 use tokens::*;
 
+enum ReplResult {
+    ExprResult(i32),
+    Quit,
+}
+
+fn read_eval() -> ReplResult {
+    print!(">>> ");
+    io::stdout().flush();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    let input = input.trim();
+
+    if input == "quit" {
+        return ReplResult::Quit;
+    }
+
+    ReplResult::ExprResult(eval(input))
+}
+
 fn main() {
-    println!("{}", eval("125 + 89  * 897    /8     +5"));
+    println!("Shunting-yard algorithm v1. By yoannd");
+    loop {
+        let result = read_eval();
+        match result {
+            ReplResult::ExprResult(res) => println!("{}", res),
+            ReplResult::Quit => break,
+        }
+    }
 }

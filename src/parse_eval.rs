6f87f8c298147257d2defc3a6e308b5769a4f83a
yoannd <yoanndewilde@gmail.com>
2020-07-09 13:44:29 +0200

    [String evaluation] Unit tests

--- src/parse_eval.rs
+++ src/parse_eval.rs
@@ -6,3 +6,35 @@ pub fn eval(input: &str) -> i32 {
 
     eval_rpn(rpn)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sum() {
+        assert_eq!(eval("1+2"), 3);
+    }
+
+    #[test]
+    fn whitespaces() {
+        assert_eq!(eval("1      +    5   *8"), 41);
+
+        assert_eq!(
+            eval("                   5             *            5       "),
+            25
+        );
+    }
+
+    #[test]
+    fn complex_expr() {
+        assert_eq!(eval("125 + 89  * 897    /8     +5"), 10_109);
+    }
+
+    #[test]
+    fn number() {
+        assert_eq!(eval("0"), 0);
+        assert_eq!(eval("3"), 3);
+        assert_eq!(eval("   12   "), 12);
+    }
+}
